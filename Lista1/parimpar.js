723e82d73cb8d1fae1da8fbde77144a4eba82db7
José Neto <64179002+JoseNNeto@users.noreply.github.com>
2023-03-09 16:13:14 -0300

    Delete Lista1 directory

--- Lista1/parimpar.js
+++
@@ -1,9 +0,0 @@
-function par_impar(numero){
-    if(numero % 2 == 0){
-        return "par";
-    }else{
-        return "ímpar";
-    }
-}
-    
-console.log(par_impar(5));
--- Lista1/soma_multiplicação.js
+++
@@ -1,9 +0,0 @@
-function soma_multiplicacao(num1, num2) {
-    if(num1 == num2) {
-        return(num1 + num2);
-    } else {
-        return (num1 * num2);
-    }
-}
-
-console.log(soma_multiplicacao(2, 6));
